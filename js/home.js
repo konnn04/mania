72f00bf38465542173b598c624a5e89b432a2f37
Nguyễn Triều <trieukon1011@gmail.com>
2023-05-11 22:35:49 +0700

    1105

---
+++ js/home.js
@@ -0,0 +1,87 @@
+const PL = document.getElementsByClassName("listplay")
+const home = document.getElementById("home")
+const gameplay = document.getElementById("gameplay")
+const titleWEB = document.querySelector("title")
+var itemSong
+var data ={}
+var select=-1
+
+//info
+var beatmapAll
+//= data[select].beatmap
+var demo
+var bpm
+
+
+
+fetchAPI()
+async function fetchAPI(){
+    await fetch('https://6433dc771c5ed06c95889a06.mockapi.io/mes/ManiaCTB', {
+        method: 'GET',
+        headers: {'content-type':'application/json'},
+      }).then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+       
+      }).then(tasks => {
+        data = tasks;
+        update(tasks)
+      }).catch(error => {
+        console.log("looix")
+      })
+}
+
+const update = (data)=>{
+    for (let i=0;i<data.length;i++) {
+        PL[0].innerHTML+=`<div class="itemsong">
+        <div class="playButton">
+            <i class="fa-solid fa-play"></i>
+        </div>
+        <div class="infoBox">
+            <p class="titleSong">${data[i].name}</p>
+            <p class="infoSong">${data[i].info}</p>
+        </div>
+    </div>`
+    }
+    itemSong = document.getElementsByClassName("itemsong")
+    playSong = document.getElementsByClassName("playButton")
+    for (let i=0;i<data.length;i++) {
+        itemSong[i].onclick =()=>{
+            //UPDATE
+            titleWEB.textContent ="Mania | "+ data[i].name
+            bpm=data[i].bpm
+            bg=data[i].bg
+            root.style.setProperty("--linkbg", `url(${bg})`)
+            //DEMO
+                if (select!=-1) {
+                    demo.pause()
+                }
+                select=i;
+                demo = new Audio(data[i].audio)
+                demo.currentTime = 40
+                demo.play()
+        }
+        playSong[i].onclick = ()=>{
+            music= new Audio(data[i].audio)
+            beatmapAll=data[i].beatmap
+            home.opacity=0
+            bpm=data[i].bpm
+            steptime = (60000/bpm)
+            setTimeout(()=>{
+                home.style.display = "none"
+                gameplay.style.display = "flex";
+                delay = speed* ((zonecorrect.offsetTop) / (line[0].offsetHeight+100))
+                gameplay.style.opacity = 0
+
+            },500)
+            setTimeout(()=>{
+                demo.pause()
+                gameplay.style.opacity = 1
+            },1500)
+        }
+    }
+        
+    
+    
+}
--- js/test.js
+++ js/test.js
@@ -1,4 +1,4 @@
-const music2 = new Audio(`https://cdn.glitch.global/dc6948cb-dff5-4efc-86db-fe258b6f1750/IDOL_TVSIZE.mp3`)
+const music2 = new Audio(`https://cdn.glitch.global/dc6948cb-dff5-4efc-86db-fe258b6f1750/MephistoTVSize.mp3`)
 var start = false
 var s="["
 var bpmCre = 161.50
@@ -6,6 +6,7 @@ document.documentElement.addEventListener('keypress',(e)=>{
     if(e.key == 'p') {
         if (start==false) {
             music2.play()
+            demo.pause()
             start=true;
         }else{
 
